25054d450c34a2d927425d52bd3d9d3e237b2101
Steve Jahns <s.t.jahns@gmail.com>
2014-01-23 19:32:52 -0700

    Physics-based conveyor belt prefab

---
+++ Assets/ConveyorBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ConveyorBehavior : MonoBehaviour
+{
+	public List<HingeJoint2D> conveyorMotors;
+	public float ConveyorSpeed;
+
+	private float oldSpeed;
+
+	void Start()
+	{
+		oldSpeed = ConveyorSpeed;
+
+		conveyorMotors.ForEach(m => {
+			JointMotor2D motor = m.motor;
+			motor.motorSpeed = ConveyorSpeed;
+			m.motor = motor;
+		});
+	}
+
+	void Update()
+	{
+		if (oldSpeed != ConveyorSpeed)
+		{
+			conveyorMotors.ForEach(m => {
+				JointMotor2D motor = m.motor;
+				motor.motorSpeed = ConveyorSpeed;
+				m.motor = motor;
+			});
+			oldSpeed = ConveyorSpeed;
+		}
+	}
+}

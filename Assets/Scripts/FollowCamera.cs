2d91412e90ff19d003779b5722506a38d9b9f5fc
aghoneim0 <aghoneim@ualberta.ca>
2014-01-28 21:52:12 -0700

    Update to Camera controls
    ^Update Camera Zoom in and out limit .M to Zoom in and N to zoom out

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,11 +6,14 @@ public class FollowCamera : MonoBehaviour {
 	
 	// Set target
 	public Transform target;
-	public int zoom = 20;
+	[HideInInspector]
+	public int zoom = 5;
+	[HideInInspector]
 	public int zoom_size=5;
+	[HideInInspector]
 	public int normal = 60;
+	[HideInInspector]
 	public float smooth =5;
-	private bool isZoomed = false;
 	
 	void  Update (){
 
@@ -23,26 +26,21 @@ public class FollowCamera : MonoBehaviour {
 
 		if (Input.GetKeyDown(KeyCode.M)) {
 			Debug.Log("True");
-
+			if (zoom <15)
+			{
 			zoom+=zoom_size;
-
+			}
 
 		} 
 		if (Input.GetKeyDown(KeyCode.N)) {
 				Debug.Log("False");
-
+			if (zoom > 0)
+			{
 			zoom-=zoom_size;
-
+			}
 			}
 
 		camera.orthographicSize= Mathf.Lerp(camera.orthographicSize,zoom,Time.deltaTime*smooth);
-		if(isZoomed == true){
-
-
-		}
-		else{
-
-		}
 
 	} 
 	

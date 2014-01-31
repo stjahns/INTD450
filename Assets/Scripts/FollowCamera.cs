8c83061f60064bee3fdc04d2ca579dfe6a598591
Steve Jahns <s.t.jahns@gmail.com>
2014-01-30 18:49:51 -0700

    Reexposed FollowCamera behaviour variables for tuning in inspector

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,47 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FollowCamera : MonoBehaviour {
+public class FollowCamera : MonoBehaviour
+{
 	
-	
-	// Set target
 	public Transform target;
-	[HideInInspector]
-	public int zoom = 5;
-	[HideInInspector]
-	public int zoom_size=5;
-	[HideInInspector]
-	public int normal = 60;
-	[HideInInspector]
-	public float smooth =5;
-	
-	void  Update (){
 
+	public float viewportHeight = 5;
+
+	public float zoomStepSize = 5;
+
+	public float zoomSmoothing = 5;
+
+	public float maxViewportHeight = 15;
+
+	public float minViewportHeight = 1;
+	
+	void Update ()
+	{
 
 		transform.position = new Vector3(
-			target.position.x, 
-			target.position.y, 
-			transform.position.z 
-				);
+				target.position.x, 
+				target.position.y, 
+				transform.position.z);
 
-		if (Input.GetKeyDown(KeyCode.M)) {
-			Debug.Log("True");
-			if (zoom <15)
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			if (viewportHeight + zoomStepSize <= maxViewportHeight)
 			{
-			zoom+=zoom_size;
+				viewportHeight += zoomStepSize;
 			}
 
 		} 
-		if (Input.GetKeyDown(KeyCode.N)) {
-				Debug.Log("False");
-			if (zoom > 0)
-			{
-			zoom-=zoom_size;
-			}
-			}
 
-		camera.orthographicSize= Mathf.Lerp(camera.orthographicSize,zoom,Time.deltaTime*smooth);
+		if (Input.GetKeyDown(KeyCode.N))
+		{
+			if (viewportHeight - zoomStepSize >= minViewportHeight)
+			{
+				viewportHeight -= zoomStepSize;
+			}
+		}
+
+		camera.orthographicSize= Mathf.Lerp(
+				camera.orthographicSize,
+				viewportHeight,
+				Time.deltaTime * zoomSmoothing);
 
 	} 
-	
+
 } 

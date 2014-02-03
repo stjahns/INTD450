e4dc427c4970d046ca9b25e0d28a249d549bc8e2
Steve Jahns <s.t.jahns@gmail.com>
2014-02-02 19:25:38 -0700

    Added link to documentation from LevelMusic prefab

--- Assets/Scripts/LevelMusic.cs
+++ Assets/Scripts/LevelMusic.cs
@@ -19,18 +19,6 @@ public class LevelMusic : MonoBehaviour
 	public static LevelMusic Instance;
 
 	//
-	// Mute or unmuted the autio track associated with the given LimbType
-	//
-	public void SetLimbTrackMuted(LimbType limb, bool muted)
-	{
-		int trackIndex = limbTypes.FindIndex(t => t == limb);
-		if (trackIndex != -1 && trackIndex < limbTracks.Count)
-		{
-			limbTracks[trackIndex].mute = muted;
-		}
-	}
-
-	//
 	// Set static instance to this, mute all limb tracks
 	// Limb tracks will be umnuted in their limb's Start() if they are attached,
 	// and will be unmuted/muted when limbs are attached/detached
@@ -56,4 +44,24 @@ public class LevelMusic : MonoBehaviour
 			track.Play();
 		}
 	}
+
+	//
+	// Allows us to set an editor icon
+	//
+	void OnDrawGizmos()
+	{
+	}
+
+	//
+	// Mute or unmuted the autio track associated with the given LimbType
+	//
+	public void SetLimbTrackMuted(LimbType limb, bool muted)
+	{
+		int trackIndex = limbTypes.FindIndex(t => t == limb);
+		if (trackIndex != -1 && trackIndex < limbTracks.Count)
+		{
+			limbTracks[trackIndex].mute = muted;
+		}
+	}
+
 }

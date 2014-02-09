2363b38b8f016b1bc0e86709d86501d18b65f60b
aghoneim0 <aghoneim@ualberta.ca>
2014-02-09 16:23:52 -0700

    Moving script Example could be seen in Tutorial Level
    Added a Moving plate script

---
+++ Assets/Scripts/moving_plates.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class moving_plates : MonoBehaviour {
+	
+	public GameObject moving_plate;
+	public float bouncespeed = 0.002f;
+	private bool itemBounceUp =true;
+	public float Max_y;
+	private float temp_max;
+	private float temp_y;
+	void Start () {
+		StartCoroutine(itembounce());
+		Vector3 temp = moving_plate.transform.position;
+		temp_y = temp.y;
+		temp_max = Max_y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = moving_plate.transform.position;
+		///Debug.Log (temp.y);
+		if (temp.y <= Max_y && itemBounceUp==true ) 
+		{
+			Max_y=temp_max;
+			temp.y += bouncespeed;
+		}
+		if (temp.y > Max_y && !itemBounceUp==false)
+		{
+			
+			Max_y=temp_y;
+			temp.y-= bouncespeed;
+		}
+		
+		moving_plate.transform.position = temp;
+	}
+	IEnumerator  itembounce () {
+		
+		yield return new WaitForSeconds (0.2f);
+		itemBounceUp = false;
+		yield return new WaitForSeconds (0.2f);
+		itemBounceUp = true;
+		
+	}
+	
+}

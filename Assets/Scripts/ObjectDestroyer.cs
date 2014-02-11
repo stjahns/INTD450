d779b2c661c046ea21b674de9965052fca72a401
Steve Jahns <s.t.jahns@gmail.com>
2014-02-11 13:29:16 -0700

    Added ObjectDestroyer component, that destroys any object matching a given list of tags on the ObjectDestroyer

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Collider2D))]
+public class ObjectDestroyer : MonoBehaviour
+{
+	public List<string> objectTags;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (objectTags.Count == 0 || 
+				objectTags.Contains(other.attachedRigidbody.gameObject.tag))
+		{
+			GameObject obj = other.attachedRigidbody.gameObject;
+			obj.SendMessage("OnDestroy", SendMessageOptions.DontRequireReceiver);
+			Destroy(obj);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (objectTags.Count == 0 || 
+				objectTags.Contains(other.rigidbody.gameObject.tag))
+		{
+			GameObject obj = other.rigidbody.gameObject;
+			obj.SendMessage("OnDestroy", SendMessageOptions.DontRequireReceiver);
+			Destroy(obj);
+		}
+	}
+}

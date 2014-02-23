aabf915e3bd68c157a7512c17302d09b3a21afd4
aghoneim0 <aghoneim@ualberta.ca>
2014-02-23 14:22:43 -0700

    Added a Main Menu Skeleton

---
+++ Assets/Menus/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	void  OnMouseEnter() {
+		double b = -20;
+		Vector3 v = new Vector3((float)b,0,0);
+		transform.Translate(v * 1 * Time.deltaTime); 
+		renderer.material.color= Color.blue;
+	}
+	
+	void  OnMouseExit() {
+		double b = 20;
+		Vector3 v = new Vector3((float)b,0,0);
+		transform.Translate(v * 1 * Time.deltaTime);
+		renderer.material.color= Color.white;
+		
+	}
+}

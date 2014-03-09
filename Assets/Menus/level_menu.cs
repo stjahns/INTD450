67631c7f8c9af89571deeb652b0c647a28672af6
aghoneim0 <aghoneim@ualberta.ca>
2014-03-09 11:51:45 -0600

    Updated the Main Menu

--- Assets/Menus/level_menu.cs
+++ Assets/Menus/level_menu.cs
@@ -5,11 +5,12 @@ using SimpleJSON;
 public class level_menu : MonoBehaviour {
 
 	// Use this for initialization
-	public Sprite level_sprite;
+	public int level_sprite;
 	public Save_Load load;
 	private string player_name="player";
 	private int current_level;
 	private int player_level;
+    private string[] current_object_name;
 
 	void Start () {
 		SpriteRenderer render = gameObject.GetComponent <SpriteRenderer>();
@@ -17,35 +18,46 @@ public class level_menu : MonoBehaviour {
 		load.player_name = player_name;
 		var data =load.file_load ();
 		Debug.Log (data);
-
+        ////string [] current_object_name;
+        current_object_name = transform.name.Split(' ');
+        Debug.Log(current_object_name[0]);
+        current_level = System.Convert.ToInt32 (current_object_name[1]);
+        
 		 player_level = System.Convert.ToInt32(data["array"][1]["Level"]);
-		///Debug.Log ("Level "+ data["array"][1]["Level"]);
+         if (player_level == 0)
+         {
+             player_level += 1;
+         }
+        ///Debug.Log ("Level "+ data["array"][1]["Level"]);
 		///Debug.Log ("Scroe "+ data["array"][1]["Score"]);
 		///Debug (System.Convert.ToInt32(gameObject.name));
-		 current_level = System.Convert.ToInt32 (transform.name);
+		 ////current_level = System.Convert.ToInt32 (transform.name);
 		Debug.Log ("Player level"+player_level);
 		Debug.Log (transform.name+"Current level"+current_level);
-		if  (player_level  >= current_level)
-		{ 
-			render.sprite = level_sprite;
+        if (current_object_name[0] == "X")
+        {
+            if (player_level >= current_level)
+            {
+                Destroy(this);
 
-		}else
-		{
-			Debug.Log("Cant advance ");
-		}
+            }
+        }
 	
 	}
-	void  OnMouseUp() {
-		if  (player_level  >= current_level)
-		{ 
-			Application.LoadLevel(current_level);
-			
-		}else
-		{
-			Debug.Log("Cant advance ");
-		}
+    void OnMouseUp()
+    {
+        if (current_object_name[0] == "level")
+        {
+            if (player_level >= current_level)
+            {
+                Application.LoadLevel(current_level);
 
-
-		}
+            }
+            else
+            {
+                Debug.Log("Cant advance ");
+            }
+        }
+    }
 
 }
---
+++ Assets/Scripts/Save_loadV2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Save_loadV2 : MonoBehaviour {
+
+	// Use this for initialization
+    
+	void Start () {
+        RobotComponent[] obj = FindObjectsOfType(typeof(RobotComponent)) as RobotComponent[];
+        ////Dictionary<string, string> game_object_components;
+        ////Dictionary<string, string> game_object_components = new Dictionary<string, string>();
+        var list = new Dictionary<string, string>();
+
+
+        foreach (RobotComponent test in obj)
+        {
+            //Instance ID is the Key
+            //The Postion 
+            string Data = test.transform.rotation.ToString() + ":" + test.transform.position.ToString();
+            list[test.GetInstanceID().ToString()] = Data;
+            
+            Debug.Log(test.GetInstanceID());
+            Debug.Log(Data);
+        }
+        
+        
+
+        ///Debug.Log(d);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   
+}

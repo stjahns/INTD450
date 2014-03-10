d5891af85121ce8b8fe4f55764d18cc4a07a2c71
Steve Jahns <s.t.jahns@gmail.com>
2014-03-09 22:50:41 -0600

    Explosive boxes and BigExplosion (explosion damages stuff)

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -6,6 +6,10 @@ public class Explosion : MonoBehaviour {
 	public AudioClip explosionClip;
 
 	public float time = 0.1f;
+	
+	public float explosionRadius = 1.0f;
+	public float explosionForce = 1.0f;
+	public int explosionDamage = 5;
 
 	void Start ()
 	{
@@ -14,7 +18,47 @@ public class Explosion : MonoBehaviour {
 			AudioSource.PlayClipAtPoint(explosionClip, transform.position);
 		}
 
-		Destroy(gameObject, 0.1f);
+		// get everything in explosion radius and apply force
+		Collider2D[] inExplosion = Physics2D.OverlapCircleAll(
+				transform.position,
+				explosionRadius);
+
+		foreach (Collider2D collider in inExplosion)
+		{
+
+			float distance = Vector2.Distance(collider.transform.position,
+					transform.position);
+
+			if (collider.rigidbody2D)
+			{
+				collider.rigidbody2D.AddForce(
+						(collider.transform.position - transform.position).normalized
+						* explosionForce
+						* (1.0f - distance / explosionRadius));
+			}
+		}
+
+		StartCoroutine(ApplyDamage());
+	}
+
+	IEnumerator ApplyDamage()
+	{
+		yield return new WaitForSeconds(time);
+
+		// get everything in explosion radius and apply damage
+		Collider2D[] inExplosion = Physics2D.OverlapCircleAll(
+				transform.position,
+				explosionRadius);
+
+		foreach (Collider2D collider in inExplosion)
+		{
+			GameObject obj = collider.gameObject;
+			obj.SendMessage("TakeDamage",
+					explosionDamage,
+					SendMessageOptions.DontRequireReceiver);
+		}
+
+		Destroy(gameObject);
 	}
 }
 

dd96f86f24ea3ee58e2e04187946421ba2dc87f0
Steve Jahns <s.t.jahns@gmail.com>
2014-03-17 09:47:29 -0600

    cannon arm has finite charges, with adjustable recharge rate

--- Assets/Scripts/Robot/CannonComponent.cs
+++ Assets/Scripts/Robot/CannonComponent.cs
@@ -11,9 +11,15 @@ public class CannonComponent : LimbComponent
 
 	public Transform shotOrigin;
 
+	public int chargeCount = 2;
+	public float chargeTime = 2.0f;
+
 	public AudioClip fireClip;
 
 	private Transform forward;
+
+	private float chargeTimer;
+	private int charges;
 
 	override public void Start ()
 	{
@@ -23,20 +29,42 @@ public class CannonComponent : LimbComponent
 		forwardObject.transform.parent = transform;
 		forwardObject.transform.localPosition = new Vector3(0, -1, 0);
 		forward = forwardObject.transform;
+
+		chargeCount = charges;
+	}
+
+	override public void Update()
+	{
+		base.Update();
+
+		if (charges < chargeCount)
+		{
+			chargeTimer += Time.deltaTime;
+			if (chargeTimer > chargeTime)
+			{
+				++charges;
+				chargeTimer = 0f;
+			}
+		}
 	}
 
 	override public void FireAbility()
 	{
-		animator.SetTrigger("Fire");
-		SFXSource.PlayOneShot(fireClip);
+		if (charges > 0)
+		{
+			animator.SetTrigger("Fire");
+			SFXSource.PlayOneShot(fireClip);
 
-		// fire cannonball
-		Vector3 direction = forward.position - transform.position;
-		direction.Normalize();
+			// fire cannonball
+			Vector3 direction = forward.position - transform.position;
+			direction.Normalize();
 
-		GameObject cannonBall = Instantiate(cannonballPrefab, shotOrigin.position, Quaternion.identity) as GameObject;
+			GameObject cannonBall = Instantiate(cannonballPrefab, shotOrigin.position, Quaternion.identity) as GameObject;
 
-		cannonBall.rigidbody2D.velocity = direction * shotVelocity;
-		
+			cannonBall.rigidbody2D.velocity = direction * shotVelocity;
+
+			charges -= 1;
+			chargeTimer = 0f;
+		}
 	}
 }

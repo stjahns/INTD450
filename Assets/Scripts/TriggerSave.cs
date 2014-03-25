8ac7acaa58c1ecd6fca8c6f9ebc4088cc89f7296
aghoneim0 <aghoneim@ualberta.ca>
2014-03-25 01:36:31 -0600

    Added Box Save and load

--- Assets/Scripts/TriggerSave.cs
+++ Assets/Scripts/TriggerSave.cs
@@ -8,12 +8,15 @@ public class TriggerSave : MonoBehaviour {
 	// Use this for initialization
 	void OnTriggerEnter2D (Collider2D other){
         string checkpoint = "";
+        string boxes_checkpoint = "";
         ///Debug.Log(other.gameObject.name);
         if (other.gameObject.name == "HED-I(Clone)")
         {
             PlayerBehavior player = PlayerBehavior.Player;
             RobotComponent[] obj = FindObjectsOfType(typeof(RobotComponent)) as RobotComponent[];
-         ///   Debug.Log("Length"+obj.Length);
+            BoxComponent[] level_boxes = FindObjectsOfType(typeof(BoxComponent)) as BoxComponent[];        
+            
+            ///   Debug.Log("Length"+obj.Length);
 		    int level = Application.loadedLevel;
 		    Save_Load save = new Save_Load ();
 		    save.player_name="player";
@@ -23,11 +26,19 @@ public class TriggerSave : MonoBehaviour {
               ////  Data += "/" + test.name + "/" + test.GetInstanceID() + ":" + test.transform.rotation.ToString() + ":" + test.transform.position.ToString() + "/";
                 checkpoint += gam.name + ":" + gam.transform.rotation.ToString() + ":" + gam.transform.position.ToString() + "/";
             }
-           /// Debug.Log(checkpoint);
+            Debug.Log(level_boxes.Length + "boxle");
+           if (level_boxes.Length >=1)
+           {
+               foreach (BoxComponent box in level_boxes)
+               {
+                   Debug.Log(box.name);
+                   boxes_checkpoint += "Box:" + box.transform.rotation.ToString() + ":" + box.transform.position.ToString() + "/";
+               }
+           }
             if (player != null)
             {
            ///     Debug.Log(level);
-                save.add_checkpoint(level, checkpoint, player.transform.position);
+                save.add_checkpoint(level, checkpoint, boxes_checkpoint, player.transform.position);
              ///   Debug.Log("Save Now" + checkpoint);
             }
         }
